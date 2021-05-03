8b17b8fc59112a042f9b205acd05a5cc24bcadb9
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-05-02 22:31:37 -0400

    Add tasks/home.html and tests

--- todo/core/tests/test_task.py
+++ todo/core/tests/test_task.py
@@ -1,5 +1,14 @@
 from django.urls import reverse
+from pytest_django.asserts import assertContains
 
 def test_home_status_code(client):
     response = client.get(reverse('core:home'))
     assert response.status_code == 200
+
+def test_contains_form(client):
+    response = client.get(reverse('core:home'))
+    assertContains(response, '<form')
+
+def test_contains_submit_btn(client):
+    response = client.get(reverse('core:home'))
+    assertContains(response, '<button type="submit"')
--- todo/core/views.py
+++ todo/core/views.py
@@ -3,4 +3,4 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Tarefas')
+    return render(request, 'tasks/home.html')
