6c278cac37e1dca4ba15b5fd67bc363652f53477
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-05-03 20:10:03 -0400

    Add success test to create a new task

--- todo/core/tests/test_task.py
+++ todo/core/tests/test_task.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from pytest_django.asserts import assertContains
+from todo.core.models import Task
 
 def test_home_status_code(client):
     response = client.get(reverse('core:home'))
@@ -12,3 +13,7 @@ def test_contains_form(client):
 def test_contains_submit_btn(client):
     response = client.get(reverse('core:home'))
     assertContains(response, '<button type="submit"')
+
+def test_task_exists_in_db(client, db):
+    response = client.post(reverse('core:home'), data={'title': 'task#1'})
+    assert Task.objects.exists()
