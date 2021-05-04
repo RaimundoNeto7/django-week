25917f3d88d5a69e5d7d5bfe56d643db9078b0f4
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-05-03 20:09:46 -0400

    Save tasks in db

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from todo.core.forms import TaskForm
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
     return render(request, 'tasks/home.html')
