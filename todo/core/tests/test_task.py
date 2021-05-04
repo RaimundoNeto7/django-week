69fabb993614c8015fe4abed1680cd225a206076
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-05-04 12:38:29 -0400

    Add tests about update status

--- todo/core/tests/test_task.py
+++ todo/core/tests/test_task.py
@@ -34,7 +34,7 @@ def test_invalid_task_status_code(client, db):
 
 
 @pytest.fixture
-def pending_tasks(db):
+def list_pending_tasks(db):
     tasks = [
         Task(title='Tarefa #1', done=False),
         Task(title='Tarefa #2', done=False),
@@ -42,7 +42,19 @@ def pending_tasks(db):
     Task.objects.bulk_create(tasks)
     return tasks
 
-def test_contains_list_tasks(client, pending_tasks):
+def test_contains_list_tasks(client, list_pending_tasks):
     response = client.get(reverse('core:home'))
-    for task in pending_tasks:
+    for task in list_pending_tasks:
         assertContains(response, task.title)
+
+@pytest.fixture
+def pending_task(db):
+    return Task.objects.create(title='Tarefa #1', done=False)
+
+def test_update_task(client, pending_task):
+    response = client.post(reverse('core:detail', kwargs={'task_id': pending_task.id}), data={'done': 'true', 'title': pending_task.title})
+    assert Task.objects.first().done
+
+def test_update_task(client, pending_task):
+    response = client.post(reverse('core:detail', kwargs={'task_id': pending_task.id}), data={'done': 'true', 'title': pending_task.title})
+    assert response.status_code == 302
